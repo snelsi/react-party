5234e33e1a3a7df22cd81c35aa885aa1646598ff
Roman Zhuravlov <SnelsiThePirate@gmail.com>
2020-03-04 22:58:03 +0200

    add pwa support

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
 
 import "styles.css";
 import App from "App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+serviceWorker.register();
